3cf258b5d4b666172198706a898f9c650bae2456
ek-sree <eksreehari05@gmail.com>
2024-02-20 21:55:41 +0530

    stack

---
+++ stack/stackArray.js
@@ -0,0 +1,97 @@
+class Stack{
+    constructor(){
+        this.items =[]
+    }
+
+    isEmpty(){
+        return this.items.length ==0
+    }
+
+    push(value){
+        this.items[this.items.length]=value
+    }
+
+    pop(){
+        if(this.isEmpty()){
+            return "UnderFlow"
+        }else{
+            let popedItem = this.items[this.items.length-1]
+            this.items.length -=1
+            return popedItem
+        }
+    } 
+
+    peek(){
+        if(this.isEmpty()){
+            return "Stack is empty"
+        }else{
+            return this.items[this.items.length-1]
+        }
+    }
+
+    minValue(){
+        if(this.isEmpty()){
+            return "stack is empty"
+        }else{
+            let min = this.items[0]
+            for(let i =0; i<this.items.length; i++){
+                if(this.items[i] < min){
+                    min = this.items[i]
+                }
+            }
+            return min
+        }
+    }
+
+    midValue(){
+        if(this.isEmpty()){
+            return "stack is empty"
+        }else{
+            let mid = Math.floor(this.items.length/2)
+            return this.items[mid]
+        }
+    }
+
+    size(){
+        return this.items.length
+    }
+
+    clear(){
+        this.items=[]
+    }
+
+    display(){
+        let result =''
+        for(let i =this.items.length-1; i>=0; i--){
+            result += this.items[i]+' '
+        }
+        console.log(result);
+    }
+}
+
+const stack = new Stack()
+
+console.log(stack.isEmpty());
+
+stack.push(10)
+stack.push(20)
+stack.push(30)
+
+stack.display()
+console.log("min...");
+console.log(stack.minValue());
+console.log(stack.midValue());
+
+console.log("peek...");
+console.log(stack.peek());
+
+console.log(stack.size());
+console.log(stack.isEmpty());
+
+stack.pop()
+stack.display()
+
+stack.clear()
+console.log(stack.isEmpty());
+console.log(stack.size());
+
---
+++ stack/stackLinkedList.js
@@ -0,0 +1,114 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class Stack{
+    constructor(){
+        this.top = null
+        this.size = 0
+    }
+
+    isEmpty(){
+        return this.size ==0
+    }
+
+    push(value){
+        const node = new Node(value)
+        if(this.isEmpty()){
+            this.top = node
+        }else{
+            node.next = this.top
+            this.top = node
+        }
+        this.size++
+    }
+
+    pop(){
+        if(this.isEmpty()){
+            return "Underflow"
+        }else{
+            let popedItem = this.top.value
+            this.top = this.top.next
+            this.size--
+            return popedItem
+        }
+    }
+
+    minValue(){
+        if(this.isEmpty()){
+            return "Stack is empty"
+        }else{
+            let current = this.top
+            let min = current.value
+            while(current){
+                if(current.value < min){
+                    min = current.value
+                }
+                current = current.next
+            }
+            return min
+        }
+    }
+
+    midValue(){
+        if(this.isEmpty()){
+            return "stack is empty"
+        }else{
+            let slow = this.top
+            let fast = this.top
+            while(fast && fast.next){
+                slow = slow.next
+                fast = fast.next.next
+            }
+            return slow.value
+
+        }
+    }
+
+    peek(){
+        if(this.isEmpty()){
+            return "Stack is empty"
+        }else{
+            return this.top.value
+        }
+    }
+
+    getSize(){
+       return this.size
+    }
+
+    display(){
+        let current = this.top
+        let result = ''
+        while(current){
+            result += current.value+' '
+            current=current.next
+        }
+        console.log(result);
+    }
+}
+
+const stack = new Stack()
+console.log(stack.isEmpty());
+console.log(stack.getSize());
+stack.push(10)
+stack.push(20)
+stack.push(30)
+console.log('min...');
+console.log(stack.minValue());
+console.log(stack.midValue());
+console.log("size....");
+console.log(stack.getSize());
+
+stack.display()
+
+console.log(stack.isEmpty());
+
+console.log("pop...");
+stack.pop()
+console.log(stack.getSize());
+
+stack.display()
